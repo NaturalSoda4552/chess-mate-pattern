d164c9396fd47fff627bc33851e8f8756371891c
GGGGGangSub <dksrkdwns1411@daum.net>
2025-11-11 22:41:16 +0900

    feat(studyChessLibrary.js): 기물 이동 로직 학습

--- study/studyChessLibrary.js
+++ study/studyChessLibrary.js
@@ -16,9 +16,55 @@ const chess2 = new Chess(
 );
 
 // 1. 체스판 상태를 FEN 표기법인 문자열로 반환
-console.log(chess1.fen());
-console.log(chess2.fen());
+// console.log(chess1.fen());
+// console.log(chess2.fen());
 
 // 2. 체스판 상태를 ascii 문자열로 반환
-console.log(chess1.ascii());
-console.log(chess2.ascii());
+// console.log(chess1.ascii());
+// console.log(chess2.ascii());
+
+// 3. 체스 기물 이동
+// 3-1. 3가지 이동 방식
+// 3-1-1. Standard Algebraic Notation (SAN)
+// 간결한 방식
+chess1.move('e4'); // w
+chess1.move('d5'); // b
+chess1.move('exd5'); // w
+chess1.move('e6'); // b
+
+// 3-1-2. Long Algebraic Notation (LAN)
+// 어디에서 어디로 이동하는지
+chess1.move('b1c3'); // w
+chess1.move('g8f6'); // b
+
+// 3-1-3. Object
+// 어디 칸에서(from) 어디 칸으로(to) 이동하는지
+// 보통 프로모션을 처리할 때 유용함.
+chess1.move({ from: 'g1', to: 'f3' }); // w
+chess1.move({ from: 'e6', to: 'd5' }); // b
+
+// 3-2. move() 성공 시 Move 객체 반환
+const moveResult = chess1.move('d4'); // w
+console.log(moveResult);
+// Move {
+//   color: 'w',                    // 수를 둔 플레이어
+//   from: 'd2',                    // 어디에서
+//   to: 'd4',                      // 어디로
+//   piece: 'p',                    // 어떤 기물
+//   captured: undefined,           // (만약 잡았다면) 잡은 기물
+//   promotion: undefined,          // (만약 프로모션한다면) 승급한 기물
+//   flags: 'b',                    // 이 수의 특성 ('b': 폰 2칸 전진, 'c': 캡처, 'k': 킹사이드 캐슬링 등)
+//   san: 'd4',                     // SAN 표기
+//   lan: 'd2d4',                   // LAN 표기
+//   before: 'rnbqkb1r/ppp2ppp/5n2/3p4/8/2N2N2/PPPP1PPP/R1BQKB1R w KQkq - 0 5',
+//   after: 'rnbqkb1r/ppp2ppp/5n2/3p4/3P4/2N2N2/PPP2PPP/R1BQKB1R b KQkq - 0 5'
+// }
+
+// 3-3. move() 실패 시 ERROR 발생
+// chess1.move('c4'); // b -> Error: Invalid move: c4 (흑 차례에 폰이 c4로 갈 수 없음)
+
+// 3-4. 이동이 모호한 경우 ERROR 발생
+// chess1.move('b5'); // w -> Error: Invalid move: b5 (c3의 나이트와 f1의 비숍이 둘 다 b5로 갈 수 있음, SAN으로 표기하면 모호함)
+
+// 3-5. 마지막 수 무르기
+chess1.undo();
