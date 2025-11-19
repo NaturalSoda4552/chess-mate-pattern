e3980bc7bdc0b7d646f8d9908cc915d3f4f3f38d
GGGGGangSub <dksrkdwns1411@daum.net>
2025-11-20 01:40:33 +0900

    feat(ChessManager.js): 이동 처리 & 정답/오답 판별 기능을 수행하는 handleMove() 구현

--- src/model/ChessManager.js
+++ src/model/ChessManager.js
@@ -1,4 +1,5 @@
 import { MatePatterns } from '../data/MatePatterns.js';
+import { ValidateSquare } from '../utils/coordinate.js';
 import Board from './Board';
 
 class ChessManager {
@@ -6,12 +7,14 @@ class ChessManager {
   #currentPattern;
   #board;
   #status; // 'idle', 'ongoing', 'correct', 'wrong'
+  #solutionStep;
 
   constructor(patterns = MatePatterns) {
     this.#patterns = patterns;
     this.#currentPattern = null;
     this.#board = new Board();
     this.#status = null;
+    this.#solutionStep = 0;
   }
 
   /**
@@ -26,6 +29,51 @@ class ChessManager {
 
     this.#board.loadFen(pattern.initialFen);
     this.#status = 'ongoing';
+  }
+
+  /**
+   * 이동 처리, 정답/오답 판별, status 업데이트를 수행한다.
+   * @param {string} fromSquare
+   * @param {string} toSquare
+   */
+  handleMove(fromSquare, toSquare) {
+    // 입력받은 칸들이 유효한지 확인
+    ValidateSquare(fromSquare);
+    ValidateSquare(toSquare);
+
+    const piece = this.#board.getPiece(fromSquare);
+    if (!piece || piece.color !== this.#board.getTurn()) {
+      console.error('기물이 없거나 플레이어 색의 기물이 아닙니다.');
+      return;
+    }
+
+    const allValidMoves = piece.getValidMoves(this.#board, fromSquare);
+    if (!allValidMoves.includes(toSquare)) {
+      console.error('이동할 수 없는 위치입니다.');
+      return;
+    }
+
+    const expectedMove = this.#currentPattern.solution[this.#solutionStep];
+    if (fromSquare !== expectedMove.from || toSquare !== expectedMove.to) {
+      console.error('오답입니다!');
+      return;
+    }
+
+    // 정답인 경우
+    this.#board.movePiece(fromSquare, toSquare);
+    this.#solutionStep++;
+    if (this.#solutionStep === this.#currentPattern.solution.length) {
+      console.log('정답입니다!');
+    }
+    return;
+  }
+
+  /**
+   * 현재 Board 객체를 반환한다.
+   * @returns {import('../model/Board.js').Board | null}
+   */
+  getBoard() {
+    return this.#board;
   }
 }
 
