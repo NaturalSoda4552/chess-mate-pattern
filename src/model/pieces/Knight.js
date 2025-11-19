60610095dec88c34b316dcfc62614bdae3d7d6f5
GGGGGangSub <dksrkdwns1411@daum.net>
2025-11-20 00:30:00 +0900

    feat(Knight.js): Knight 클래스 getValidMoves() 구현

--- src/model/pieces/Knight.js
+++ src/model/pieces/Knight.js
@@ -1,3 +1,4 @@
+import { coordsToSquare, squareToCoords } from '../../utils/coordinate';
 import Piece from '../Piece';
 
 class Knight extends Piece {
@@ -10,6 +11,31 @@ class Knight extends Piece {
    */
   getValidMoves(board, fromSquare) {
     const moves = [];
+
+    const { row, col } = squareToCoords(fromSquare);
+    const directions = [
+      [-1, -2],
+      [-2, -1],
+      [1, -2],
+      [2, -1],
+      [-1, 2],
+      [-2, 1],
+      [1, 2],
+      [2, 1],
+    ];
+
+    directions.forEach((direction) => {
+      const newRow = row + direction[0];
+      const newCol = col + direction[1];
+
+      const targetSqaure = coordsToSquare(newRow, newCol);
+      if (targetSqaure) {
+        const targetPiece = board.getPiece(targetSqaure);
+
+        if (!targetPiece || targetPiece.color !== this.color)
+          moves.push(targetSqaure);
+      }
+    });
 
     return moves;
   }
