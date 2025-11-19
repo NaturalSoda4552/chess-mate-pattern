fe0862c7ea011697c7156694f71b35c96e4b076b
GGGGGangSub <dksrkdwns1411@daum.net>
2025-11-20 00:40:12 +0900

    feat(Rook.js): Rook 클래스 getValidMoves() 구현

--- src/model/pieces/Rook.js
+++ src/model/pieces/Rook.js
@@ -1,3 +1,4 @@
+import { coordsToSquare, squareToCoords } from '../../utils/coordinate';
 import Piece from '../Piece';
 
 class Rook extends Piece {
@@ -10,6 +11,44 @@ class Rook extends Piece {
    */
   getValidMoves(board, fromSquare) {
     const moves = [];
+
+    const { row, col } = squareToCoords(fromSquare);
+    const directions = [
+      [-1, 0],
+      [0, -1],
+      [0, 1],
+      [1, 0],
+    ];
+
+    directions.forEach((direction) => {
+      let distance = 1;
+
+      while (true) {
+        const newRow = row + direction[0] * distance;
+        const newCol = col + direction[1] * distance;
+
+        const targetSqaure = coordsToSquare(newRow, newCol);
+        // 1. 칸이 존재하지 않는 경우 다음 방향으로
+        if (!targetSqaure) break;
+
+        const targetPiece = board.getPiece(targetSqaure);
+        // 2. 빈 칸인 경우 추가 후 다음 칸으로 이동
+        if (!targetPiece) {
+          moves.push(targetSqaure);
+          distance += 1;
+        } else {
+          // 3. 상대 색 기물인 경우
+          if (targetPiece.color !== this.color) {
+            moves.push(targetSqaure);
+            break;
+          }
+          // 4. 같은 색 기물인 경우
+          if (targetPiece.color === this.color) {
+            break;
+          }
+        }
+      }
+    });
 
     return moves;
   }
