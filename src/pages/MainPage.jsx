c520d7b95695820dd4812b75939c3eaedf832013
GGGGGangSub <dksrkdwns1411@daum.net>
2025-11-21 01:09:15 +0900

    feat(MainPage.jsx): MainPage 페이지 Material-UI를 사용하여 구현

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,23 +1,39 @@
-import styled from 'styled-components';
 import Header from '../components/Header';
 import PatternSelector from '../components/PatternSelector';
 import Board from '../components/Board';
 import InfoPanel from '../components/InfoPanel';
 
+import { MatePatterns } from '../data/MatePatterns.js';
+
+import styled from 'styled-components';
+import { Grid, Box } from '@mui/material';
+
 const MainContainer = styled.div`
-  color: black;
+  display: flex;
 `;
 
 const title = '체스 메이트 패턴 연습하기';
 
 const MainPage = () => {
+  const handlePatternSelect = (patternId) => {
+    console.log(`Selected pattern ID: ${patternId}`);
+  };
   return (
-    <MainContainer>
+    <Box sx={{ padding: 2 }}>
       <Header title={title} />
-      <PatternSelector />
-      <Board />
-      <InfoPanel />
-    </MainContainer>
+      <Grid container spacing={3} sx={{ mt: 2 }}>
+        <Grid item xs={4}>
+          <PatternSelector
+            patterns={MatePatterns}
+            onPatternSelect={handlePatternSelect}
+          />
+        </Grid>
+        <Grid item xs={8}>
+          <Board />
+          <InfoPanel />
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
