21d7638772e6908967fd8e5f572b88f8a65779bf
GGGGGangSub <dksrkdwns1411@daum.net>
2025-11-22 01:40:31 +0900

    refactor(InfoPanel.jsx): InfoPanel 컴포넌트 디자인 수정

--- src/components/InfoPanel.jsx
+++ src/components/InfoPanel.jsx
@@ -1,5 +1,11 @@
 import { Paper, Typography, Box } from '@mui/material';
 import EmojiObjectsIcon from '@mui/icons-material/EmojiObjects';
+import styled from 'styled-components';
+
+const InfoPanelContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
 
 const InfoPanel = ({ pattern }) => {
   const title = !pattern ? '퀴즈를 선택하세요.' : pattern.name;
@@ -9,31 +15,53 @@ const InfoPanel = ({ pattern }) => {
   const hint = !pattern ? null : pattern.hint;
 
   return (
-    <Paper
-      elevation={2}
-      sx={{
-        padding: 3,
-        borderRadius: 3,
-        borderLeft: '6px solid #6366F1',
-        display: 'flex',
-        minWidth: '600px',
-        gap: 2,
-        alignItems: 'start',
-      }}
-    >
-      <EmojiObjectsIcon sx={{ color: '#6366F1', fontSize: 30 }} />
-      <Box>
-        <Typography variant="h6" fontWeight="bold" gutterBottom>
-          {title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {description}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {hint}
-        </Typography>
-      </Box>
-    </Paper>
+    <InfoPanelContainer>
+      <Paper
+        elevation={2}
+        sx={{
+          padding: 3,
+          borderRadius: 3,
+          borderLeft: '6px solid #6366F1',
+          display: 'flex',
+          gap: 2,
+          minWidth: '450px',
+          alignItems: 'start',
+        }}
+      >
+        <EmojiObjectsIcon sx={{ color: '#6366F1', fontSize: 30 }} />
+        <Box>
+          <Typography variant="h6" fontWeight="bold" gutterBottom>
+            {title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {description}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {hint}
+          </Typography>
+        </Box>
+      </Paper>
+
+      <Paper
+        elevation={2}
+        sx={{
+          padding: 3,
+          borderRadius: 3,
+          borderLeft: '6px solid #6366F1',
+          display: 'flex',
+          gap: 2,
+          minWidth: '350px',
+          alignItems: 'start',
+        }}
+      >
+        <EmojiObjectsIcon sx={{ color: '#6366F1', fontSize: 30 }} />
+        <Box>
+          <Typography variant="h6" fontWeight="bold" gutterBottom></Typography>
+          <Typography variant="body2" color="text.secondary"></Typography>
+          <Typography variant="body2" color="text.secondary"></Typography>
+        </Box>
+      </Paper>
+    </InfoPanelContainer>
   );
 };
 
