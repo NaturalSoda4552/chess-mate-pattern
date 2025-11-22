3277f3ada52b38fbc3014fe301cd8eafde629e9d
GGGGGangSub <dksrkdwns1411@daum.net>
2025-11-22 19:08:31 +0900

    refactor(Header.jsx): Header 컴포넌트 아이콘 추가 및 title 폰트 & 색 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,19 @@
 import { AppBar, Toolbar, Typography } from '@mui/material';
-import styled from 'styled-components';
-
 import bK from '../assets/bK.png';
-
-const ImageBox = styled.img`
-  alt: 'blackKing';
-  width: 40px;
-`;
 
 const Header = ({ title }) => {
   return (
-    <AppBar
-      position="static"
-      color="default"
-      sx={{
-        backgroundColor: '#ffffff',
-      }}
-    >
+    <AppBar position="static" color="default">
       <Toolbar sx={{ gap: '10px' }}>
-        <ImageBox src={bK} />
+        <img src={bK} style={{ width: '40px' }} />
         <Typography
           variant="h6"
           noWrap
           component="div"
           sx={{
             fontWeight: 800,
-            color: '#1e293b', // 진한 네이비/블랙 (Slate-900)
-            fontFamily: '"Pretendard", "Roboto", sans-serif', // 폰트 설정
-            letterSpacing: '-0.5px',
+            color: '#1e293b',
+            fontFamily: '"Pretendard", "Roboto", sans-serif',
           }}
         >
           {title}
